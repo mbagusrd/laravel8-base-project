7263cf1095ef40016ae0639660602c9baea0e8cb
bagus <mbagusrd@gmail.com>
2023-02-02 17:08:46 +0700

    update color themes for admin page

--- tailwind.config.js
+++ tailwind.config.js
@@ -17,4 +17,22 @@ module.exports = {
     },
 
     plugins: [require('@tailwindcss/forms'), require('@tailwindcss/typography'), require('daisyui')],
+
+    daisyui: {
+        themes: [
+            {
+                emerald: {
+                    "primary": "#007bff",
+                    "secondary": "#7B92B2",
+                    "accent": "#67CBA0",
+                    "neutral": "#181A2A",
+                    "base-100": "#FFFFFF",
+                    "info": "#3ABFF8",
+                    "success": "#28a745",
+                    "warning": "#FBBD23",
+                    "error": "#dc3545",
+                }
+            }
+        ],
+    },
 };
